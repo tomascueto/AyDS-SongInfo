4f5439c50eeba94085daa4b99c9f81d8b8d55b30
Emmanuel <elagarrigue@gmail.com>
2024-04-07 15:42:28 -0300

    solid refactor

--- app/src/main/java/ayds/songinfo/home/model/repository/external/spotify/tracks/SpotifyToSongResolver.kt
+++ app/src/main/java/ayds/songinfo/home/model/repository/external/spotify/tracks/SpotifyToSongResolver.kt
@@ -16,6 +16,7 @@ private const val ARTISTS = "artists"
 private const val ALBUM = "album"
 private const val IMAGES = "images"
 private const val RELEASE_DATE = "release_date"
+private const val RELEASE_DATE_PRECISION = "release_date_precision"
 private const val URL = "url"
 private const val EXTERNAL_URL = "external_urls"
 private const val SPOTIFY = "spotify"
@@ -62,7 +63,7 @@ internal class JsonToSongResolver : SpotifyToSongResolver {
 
     private fun JsonObject.getReleaseDatePrecision(): String {
         val album = this[ALBUM].asJsonObject
-        return album["release_date_precision"].asString
+        return album[RELEASE_DATE_PRECISION].asString
     }
 
     private fun JsonObject.getImageUrl(): String {
